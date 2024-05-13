2514d7ac4a1590b04ebc53e61fb4bfb211ac9a18
Mix <32300164+mnixry@users.noreply.github.com>
2024-05-13 23:55:09 +0800

    chore: Add Tailwind CSS configuration files and dependencies

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,17 +1,20 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
-import type { Theme } from 'vitepress'
-import DefaultTheme from 'vitepress/theme'
-import './style.css'
+import { h } from "vue";
+import type { Theme } from "vitepress";
+import DefaultTheme from "vitepress/theme";
+import "./style.css";
+import "./tailwind.postcss"
+
+import GitBookHint from "./components/gitbook-hint.vue";
 
 export default {
   extends: DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
+    });
   },
   enhanceApp({ app, router, siteData }) {
-    // ...
-  }
-} satisfies Theme
+    app.component('GitBookHint', GitBookHint);
+  },
+} satisfies Theme;
